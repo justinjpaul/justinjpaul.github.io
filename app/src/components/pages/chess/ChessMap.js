bf4be1d94c5ee5f325ff1df142c454f53e00229e
justinjpaul <justpaul@umich.edu>
2023-05-20 14:46:35 -0400

    add map of tourneys to chess page

---
+++ app/src/components/pages/chess/ChessMap.js
@@ -0,0 +1,113 @@
+import 'mapbox-gl/dist/mapbox-gl.css'
+import React, { useEffect, useState, useMemo } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import GetData from '../../shared/GetData';
+
+const ChessMap = () => {
+
+  const mapStyle = {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  };
+
+  const mapContainerStyle = {
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    paddingBottom: '80%'
+  };
+
+  const [popupInfo, setPopupInfo] = useState(null);
+  const [data, setData] = useState([]);
+
+  const url = '/tourneys_fuzzed.json'
+  useEffect(() => {
+    GetData({ url, setData });
+  }, []);
+
+  const handleMarkerClick = (tourney) => {
+    setPopupInfo(tourney);
+  }
+
+  function colorSelector(placement){
+    if (placement==1) {return "#FFD700"}
+    if (placement==2) {return "#C0C0C0"}
+    if (placement==2) {return "#CD7F32"}
+    return "#3FB1CE"
+  }
+
+  const pins = useMemo(
+    () =>
+      data.map((tourney, index) => (
+        <Marker
+          key={`maker-${index}`}
+          latitude={tourney.lat}
+          color={colorSelector(tourney.final_rank)}
+          longitude={tourney.long}
+          anchor="bottom"
+          onClick={e => {
+            e.originalEvent.stopPropagation();
+            handleMarkerClick(tourney);
+          }}
+        />
+      )),
+    [data]
+  );
+
+
+
+  function ordinal_suffix_of(i) {
+    var j = i % 10,
+      k = i % 100;
+    if (j == 1 && k != 11) {
+      return i + "st";
+    }
+    if (j == 2 && k != 12) {
+      return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  }
+
+  return (
+    <div style={mapContainerStyle}>
+      <ReactMapGL
+        style={mapStyle}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        initialViewState={{
+          longitude: -40,
+          latitude: 35,
+          zoom: 1
+        }}
+      >
+        {pins}
+        {popupInfo && (
+          <Popup
+            latitude={popupInfo.lat}
+            longitude={popupInfo.long}
+            onClose={() => setPopupInfo(null)}
+            anchor="top"
+          >
+            <div className="popup">
+              <b><a href={popupInfo.link} target="_blank">{popupInfo.event}</a></b>
+              <br />
+              {popupInfo.date}
+              <br />
+              Placed {ordinal_suffix_of(popupInfo.final_rank)} out of {popupInfo.participants} players
+              <br />
+              {popupInfo['pre-rating']} {"=>"} {popupInfo['post-rating']}
+            </div>
+          </Popup>
+        )}
+
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default ChessMap;
--- app/src/components/pages/chess/ChessPage.js
+++ app/src/components/pages/chess/ChessPage.js
@@ -1,12 +1,66 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from '../../shared/Header';
+import GetData from '../../shared/GetData';
+import ChessMap from './ChessMap';
+
+import './chess.css'
+
+function format_elem(li) {
+    return (
+        <>
+            <ul style={{ "listStyleType": "none", "margin": "none", "justifyContent": "center" }}>
+                {li.map(function (elem, i) {
+                    return (
+                        <li margin-inline-block="10em" overflow-wrap="true" key={`${elem}:${i}`} >{elem}</li>
+                    );
+                })}
+            </ul >
+        </>
+    );
+}
+
+
+
+function MakeStats({ data }) {
+    if (!data) {
+        return null;
+    }
+
+
+    return (
+        data.map(function (elem, i) {
+            return (
+                <div className="chess-stats" key={`${elem}:${i}`}>
+                    {format_elem(elem)}
+                </div>
+            );
+        })
+    );
+}
 
 
 export default function ChessPage() {
     const page = 'chess';
+    const url = "/chess_stats.json";
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        GetData({ url, setData });
+        console.log(data)
+    }, []);
+
 
     return <>
-    <Header page={page}/>
-    <div height="90vh">chess stuff</div>
+        <Header page={page} />
+        <div className="chess-container">
+            <div className="left-component">
+                {data.length > 0 && <MakeStats data={data} />}
+            </div>
+            <div className="right-component">
+                <ChessMap />
+
+            </div>
+
+        </div>
     </>
 };
