fdfb10a01f3cac22ed852a3a7496849f509ec11d
justinjpaul <justpaul@umich.edu>
2023-06-09 01:17:35 -0400

    make gh routing compatible

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import AboutPage from './components/pages/about/AboutPage';
 import ChessPage from './components/pages/chess/ChessPage';
