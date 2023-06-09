8cd5b3942a35c56ea200324c5c9794da0b15f216
justinjpaul <justpaul@umich.edu>
2023-06-08 20:43:12 -0400

    update header to include mobile view

--- app/src/components/shared/Header.js
+++ app/src/components/shared/Header.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import './header.css';
 
 export default function Header(props) {
     const [about, chess, portfolio, updates] = ['', 'chess', 'portfolio', 'updates'];
+    const [viewNavDropdown, setViewNavDropdown] = useState(false);
 
     function redirect(page) {
         window.location.href = `/${page}`;
@@ -9,23 +11,53 @@ export default function Header(props) {
 
 
     return <div className="header">
-        <div className="header-panel">
+        <div className={`header-panel${viewNavDropdown ? ' extend-header' : ''}`}>
             {/* <div className="button-container"> */}
-                <button onClick={() => redirect(about)} className="header-button" disabled={props.page===''}><b>About</b></button>
-                <button onClick={() => redirect(chess)} className="header-button" disabled={props.page==='chess'}><b>Chess</b></button>
-                <button onClick={() => redirect(portfolio)} className="header-button" disabled={props.page==='portfolio'}><b>Portfolio</b></button>
-                <button onClick={() => redirect(updates)} className="header-button" disabled={props.page==='updates'}><b>Updates</b></button>
-                <div className="socials">
-                    <div className="button-social">
-                        <a href="mailto:justpaul@umich.edu" target="about:blank"><i className="far fa-envelope fa-lg"></i></a>
-                    </div>
-                    <div className="button-social">
-                        <a href="https://github.com/justinjpaul" target="about:blank"><i className="fab fa-github fa-lg"></i></a>
-                    </div>
-                    <div className="button-social">
-                        <a href="https://www.linkedin.com/in/jpaul24/" target="about:blank"><i className="fab fa-linkedin fa-lg"></i></a>
-                    </div>
+            <div className='mobile'>
+                {<span className={`button-container-mobile-${viewNavDropdown ? 'hidden' :'open'}`}>
+                    <a className={'header-button'} onClick={() => {setViewNavDropdown(true); console.log(viewNavDropdown)}}>&#9776;</a>
+                </span>}
+                <ul className={`button-container-mobile${viewNavDropdown ? ' mobile-dropdown-show' : ''}`}>
+                    <li className='nav-button'>
+                        <a className={`header-button${props.page == '' ? ' disabled-button' : ''}`} onClick={() => redirect(about)}>About</a>
+                    </li>
+                    <li className='nav-button'>
+                        <a className={`header-button${props.page == 'chess' ? ' disabled-button' : ''}`} onClick={() => redirect(chess)}>Chess</a>
+                    </li>
+                    <li className='nav-button'>
+                        <a className={`header-button${props.page == 'portfolio' ? ' disabled-button' : ''}`} onClick={() => redirect(portfolio)}>Portfolio</a>
+                    </li>
+                    <li className='nav-button'>
+                        <a className={`header-button${props.page == 'updates' ? ' disabled-button' : ''}`} onClick={() => redirect(updates)}>Updates</a>
+                    </li>
+                </ul>
+            </div>
+            <ul className='button-container'>
+                <li className='nav-button'>
+                    <a className={`header-button${props.page == '' ? ' disabled-button' : ''}`} onClick={() => redirect(about)}>About</a>
+                </li>
+                <li className='nav-button'>
+                    <a className={`header-button${props.page == 'chess' ? ' disabled-button' : ''}`} onClick={() => redirect(chess)}>Chess</a>
+                </li>
+                <li className='nav-button'>
+                    <a className={`header-button${props.page == 'portfolio' ? ' disabled-button' : ''}`} onClick={() => redirect(portfolio)}>Portfolio</a>
+                </li>
+                <li className='nav-button'>
+                    <a className={`header-button${props.page == 'updates' ? ' disabled-button' : ''}`} onClick={() => redirect(updates)}>Updates</a>
+                </li>
+            </ul>
+
+            <div className="socials">
+                <div className="button-social">
+                    <a href="mailto:justpaul@umich.edu" target="about:blank"><i className="far fa-envelope fa-lg"></i></a>
                 </div>
+                <div className="button-social">
+                    <a href="https://github.com/justinjpaul" target="about:blank"><i className="fab fa-github fa-lg"></i></a>
+                </div>
+                <div className="button-social">
+                    <a href="https://www.linkedin.com/in/jpaul24/" target="about:blank"><i className="fab fa-linkedin fa-lg"></i></a>
+                </div>
+            </div>
             {/* </div> */}
         </div>
     </div>
