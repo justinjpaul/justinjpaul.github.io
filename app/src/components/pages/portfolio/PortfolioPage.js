6b6dadc116a2f343835533f2651b7c9c85b31578
justinjpaul <justpaul@umich.edu>
2023-06-08 22:07:53 -0400

    lint and cleanup

--- app/src/components/pages/portfolio/PortfolioPage.js
+++ app/src/components/pages/portfolio/PortfolioPage.js
@@ -1,16 +1,16 @@
 import Header from '../../shared/Header';
 import Projects from './Projects';
 import "./portfolio.css";
-import GitHubCalendar from 'react-github-calendar'
+// import GitHubCalendar from 'react-github-calendar'
 
-function GitDisplay() {
-    return (
-        <>
-            <h1>GitHub Profile</h1>
-            <GitHubCalendar username="justinjpaul"/>
-        </>
-    )
-}
+// function GitDisplay() {
+//     return (
+//         <>
+//             <h1>GitHub Profile</h1>
+//             <GitHubCalendar username="justinjpaul"/>
+//         </>
+//     )
+// }
 
 
 export default function PortfolioPage() {
--- app/src/components/shared/Header.js
+++ app/src/components/shared/Header.js
@@ -6,10 +6,6 @@ export default function Header(props) {
     const [viewNavDropdown, setViewNavDropdown] = useState(false);
     const dropdownRef = useRef(null);
 
-    function redirect(page) {
-        window.location.href = `/${page}`;
-    }
-
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (
@@ -33,35 +29,35 @@ export default function Header(props) {
             {/* <div className="button-container"> */}
             <div className='mobile'>
                 {<span ref={dropdownRef} className={`button-container-mobile-${viewNavDropdown ? 'hidden' :'open'}`}>
-                    <a className={'header-button'} onClick={() => {setViewNavDropdown(true); console.log(viewNavDropdown)}}>&#9776;</a>
+                    <span className={'header-button'} onClick={() => {setViewNavDropdown(true); console.log(viewNavDropdown)}}>&#9776;</span>
                 </span>}
                 <ul className={`button-container-mobile${viewNavDropdown ? ' mobile-dropdown-show' : ''}`}>
                     <li className='nav-button'>
-                        <a className={`header-button${props.page === '' ? ' disabled-button' : ''}`} onClick={() => redirect(about)}>About</a>
+                        <a className={`header-button${props.page === about ? ' disabled-button' : ''}`} href='/'>About</a>
                     </li>
                     <li className='nav-button'>
-                        <a className={`header-button${props.page === 'chess' ? ' disabled-button' : ''}`} onClick={() => redirect(chess)}>Chess</a>
+                        <a className={`header-button${props.page === chess ? ' disabled-button' : ''}`} href='/chess'>Chess</a>
                     </li>
                     <li className='nav-button'>
-                        <a className={`header-button${props.page === 'portfolio' ? ' disabled-button' : ''}`} onClick={() => redirect(portfolio)}>Portfolio</a>
+                        <a className={`header-button${props.page === portfolio ? ' disabled-button' : ''}`} href='/portfolio'>Portfolio</a>
                     </li>
                     <li className='nav-button'>
-                        <a className={`header-button${props.page === 'updates' ? ' disabled-button' : ''}`} onClick={() => redirect(updates)}>Updates</a>
+                        <a className={`header-button${props.page === updates ? ' disabled-button' : ''}`} href='/updates'>Updates</a>
                     </li>
                 </ul>
             </div>
             <ul className='button-container'>
                 <li className='nav-button'>
-                    <a className={`header-button${props.page === '' ? ' disabled-button' : ''}`} onClick={() => redirect(about)}>About</a>
+                    <a className={`header-button${props.page === about ? ' disabled-button' : ''}`} href='/'>About</a>
                 </li>
                 <li className='nav-button'>
-                    <a className={`header-button${props.page === 'chess' ? ' disabled-button' : ''}`} onClick={() => redirect(chess)}>Chess</a>
+                    <a className={`header-button${props.page === chess ? ' disabled-button' : ''}`} href='/chess'>Chess</a>
                 </li>
                 <li className='nav-button'>
-                    <a className={`header-button${props.page === 'portfolio' ? ' disabled-button' : ''}`} onClick={() => redirect(portfolio)}>Portfolio</a>
+                    <a className={`header-button${props.page === portfolio ? ' disabled-button' : ''}`} href='/portfolio'>Portfolio</a>
                 </li>
                 <li className='nav-button'>
-                    <a className={`header-button${props.page === 'updates' ? ' disabled-button' : ''}`} onClick={() => redirect(updates)}>Updates</a>
+                    <a className={`header-button${props.page === updates ? ' disabled-button' : ''}`} href='/updates'>Updates</a>
                 </li>
             </ul>
 
